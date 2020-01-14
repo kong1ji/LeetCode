5924350f0392ee5f8145cc24eb48e92796d0f04f
chen.zhihui <chen.zhihui2@iwhalecloud.com>
2020-01-14 10:31:01 +0800

    15提三数之和提交

--- Problems/0015/3 Sum.java
+++ Problems/0015/3 Sum.java
@@ -1,62 +1,54 @@
+import java.util.*;
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> list = new ArrayList<>();
-        for(int i=0;i<nums.length-2;i++){
-            for(int j=i+1;j<nums.length-1;j++){
-                for(int k=j+1;k<nums.length;k++){
-                    if(nums[i]+nums[j]+nums[k] == 0){
-                        List<Integer> intList = new ArrayList<>();
-                        boolean flag = true;
-                        intList.add(nums[i]);
-                        intList.add(nums[j]);
-                        intList.add(nums[k]);
-                        Collections.sort(intList);
-                        if(list.size() == 0){
-                            list.add(intList);
+        int[] sortNum = sortNum(nums);
+        return combineNum(sortNum);
+    }
+
+    // 排序
+    private static int[] sortNum(int[] nums) {
+        int s;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] > nums[j]) {
+                    s = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = s;
+                }
+            }
+        }
+        return nums;
+    }
+
+    // 组合
+    private static List<List<Integer>> combineNum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        List<Integer> integers = new ArrayList<>();
+                        integers.add(nums[i]);
+                        integers.add(nums[j]);
+                        integers.add(nums[k]);
+                        if (isReAdd(integers, res)) {
+                            res.add(integers);
                         }
-                        for(int m=0;m<list.size();m++){
-                            if(list.get(m).equals(intList)){
-                                flag = false;
-                                break;
-                            }
-                        }
-                        if(flag){
-                            list.add(intList);
-                        }
-                        flag = true;
                     }
                 }
             }
         }
-        return list;
+        return res;
     }
-}
 
-
-class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> list = new ArrayList<>();
-        List<Integer> intList = new ArrayList<>();
-        intList.add(0);
-        intList.add(0);
-        intList.add(0);
-        List<Integer> intList2 = new ArrayList<>();
-        intList2.add(-4);
-        intList2.add(4);
-        intList2.add(0);
-        if(intList.containsAll(intList2)){
-            list.add(intList);
+    // 去重
+    private static boolean isReAdd(List<Integer> integers, List<List<Integer>> res) {
+        for (List<Integer> iList : res) {
+            if (iList.equals(integers)) {
+                return false;
+            }
         }
-        return list;
+        return true;
     }
 }
-
-[-4,-2,1,-5,-4,-4,4,-2,0,4,0,-2,3,1,-5,0]
-        输出
-        [[-4,1,3],[-4,4,0],[-2,1,1],[-2,4,-2],[1,-5,4]]
-        预期结果
-        [[-5,1,4],[-4,0,4],[-4,1,3],[-2,-2,4],[-2,1,1],[0,0,0]]
-
-        [[-4,1,3],[-4,1,3],[-4,4,0],[-4,4,0],[-4,4,0],[-4,0,4],[-4,4,0],[-4,4,0],[-4,3,1],[-2,1,1],[-2,4,-2],[-2,4,-2],[-2,-2,4],
-        [-2,4,-2],[1,-5,4],[1,-5,4],[1,-4,3],[1,-4,3],[1,4,-5],[1,-2,1],[1,4,-5],[1,-2,1],[-5,4,1],[-5,4,1],[-4,4,0],[-4,4,0],[-4,4,0],[-4,0,4],[-4,4,0],[-4,4,0],[-4,3,1],
-        [-4,4,0],[-4,4,0],[-4,4,0],[-4,0,4],[-4,4,0],[-4,4,0],[-4,3,1],[4,-2,-2],[4,1,-5],[-2,4,-2],[0,0,0],[4,1,-5]]
